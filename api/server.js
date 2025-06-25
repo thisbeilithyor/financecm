098310484d6e3e51731775b55f0cbaac09f29ef8
chris <christoph21.schweiger@gmx.de>
2025-06-25 11:53:00 +0200

    removed unnecessary functions in server.js

--- api/server.js
+++ api/server.js
@@ -7,32 +7,6 @@ const port = 5000;
 const appName = process.env.APP_NAME;
 
 app.use('/api/sendTime', (req, res) => {
-    console.log("worked");
-    res.json({message: "success"});
-})
-
-app.use('api/api/sendTime', (req, res) => {
-    console.log("worked");
-    res.json({message: "success"});
-})
-
-app.use('/api/api/sendTime', (req, res) => {
-    console.log("worked");
-    res.json({message: "success"});
-})
-
-app.use('sendTime', (req, res) => {
-    console.log("worked");
-    res.json({message: "success"});
-})
-
-app.use('/sendTime', (req, res) => {
-    console.log("worked");
-    res.json({message: "success"});
-})
-
-app.use('api/sendTime', (req, res) => {
-    console.log("worked");
     res.json({message: "success"});
 })
 
