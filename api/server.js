8c59dee483039e19910fdcd4d38c4eec174b51c0
chris <christoph21.schweiger@gmx.de>
2025-06-25 11:48:46 +0200

    nginx webserver hosting static react files & proxy for api

--- api/server.js
+++ api/server.js
@@ -4,9 +4,34 @@ const mysql = require('mysql');
 const app = express();
 const port = 5000;
 
-const appName = process.env.APP_NAME
+const appName = process.env.APP_NAME;
 
 app.use('/api/sendTime', (req, res) => {
+    console.log("worked");
+    res.json({message: "success"});
+})
+
+app.use('api/api/sendTime', (req, res) => {
+    console.log("worked");
+    res.json({message: "success"});
+})
+
+app.use('/api/api/sendTime', (req, res) => {
+    console.log("worked");
+    res.json({message: "success"});
+})
+
+app.use('sendTime', (req, res) => {
+    console.log("worked");
+    res.json({message: "success"});
+})
+
+app.use('/sendTime', (req, res) => {
+    console.log("worked");
+    res.json({message: "success"});
+})
+
+app.use('api/sendTime', (req, res) => {
     console.log("worked");
     res.json({message: "success"});
 })
