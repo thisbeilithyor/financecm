0bc4d9bdd1c8c22e8997d61923daece2647f64ee
chris <christoph21.schweiger@gmx.de>
2025-07-07 16:15:12 +0200

    login in its own file (cleaner code)

---
+++ api/auth.js
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import User from "./database/models/user.js";
+
+const admin_login = async(req, res) => {
+    let bodySet = false;
+    let currentUsername, currentPassword;
+    if(req.body){
+        const { username, password } = req.body;
+        currentUsername = username;
+        currentPassword = password;
+        bodySet = true;
+    }
+    let user;
+    if(bodySet){
+        user = await query_database(currentUsername, currentPassword);
+    }
+
+    if(user){
+        const token = generate_jwt_token(user, currentUsername);
+        res.json({ token });
+    } else {
+        res.json({ message: "Falsche Anmeldedaten!" });
+    }
+}
+
+const generate_jwt_token = (user, currentUsername) => {
+    return jwt.sign(
+        { id: user.id, username: currentUsername },
+        "aslfjindid"
+    )
+}
+
+const query_database = async (currentUsername, currentPassword) => {
+    return await User.findOne({ 
+        where: {
+            username: currentUsername,
+            password: currentPassword,
+            admin: true
+        }
+    })
+}
+
+export default admin_login;
--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ import testModel from "./database/models/test.js";
 import User from "./database/models/user.js";
 import sequelize from "./database/util/database.js";
 import jwt from "jsonwebtoken";
+import admin_login from "./auth.js";
 
 const app = express();
 app.use(express.json());
@@ -21,77 +22,8 @@ sequelize.sync({ force: true})
 .then((result) => {
     return User.create({ username: "administrator", admin: true, password: "1111" });
 })
-.then(customer => {
-    customerId = customer.id;
-    console.log(customer);
-})
-.catch(err => {
-    console.log("ERROR");
-    console.log(err);
-});
 
-app.use('/api/sendTime', authMiddleware, async (req, res) => {
-    if(customerId){
-        c = await testModel.findAll({ where: 2 });
-    }
-    res.json({message: "success", allC: c});
-})
-
-function authMiddleware(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).json({ error: "No token provided" });
-    } 
-
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, "aslfjindid", (err, decoded) => {
-        if (err) return res.status(401).json( {error: "Token invalid"} );
-        console.log(req);
-        console.log(decoded);
-        console.log(err);
-        console.log(req.user);
-        req.user = decoded;
-        console.log(req.user);
-    })
-}
-
-app.post('/api/admin/login', async(req, res) => {
-    let bodySet = false;
-    let currentUsername, currentPassword;
-    if(req.body){
-        const { username, password } = req.body;
-        currentUsername = username;
-        currentPassword = password;
-        console.log(username);
-        console.log(password);
-        bodySet = true;
-    }
-    console.log(currentUsername);
-    let user;
-    if(bodySet){
-        user = await User.findOne({ 
-            where: {
-                username: currentUsername,
-                password: currentPassword,
-                admin: true
-            }
-        })
-    }
-
-    if(user){
-        console.log("Login erfolgreich für ", currentUsername);
-
-        const token = jwt.sign(
-            { id: user.id, username: currentUsername },
-            "aslfjindid"
-        )
-
-        res.json({ token });
-    } else {
-        res.json({ message: "Falsche Anmeldedaten!" });
-        console.log("Falsche credentials!");
-    }
-})
+app.post('/api/admin/login', admin_login);
 
 app.use('/api/verifyJWT', (req, res) => {
     const authHeader = req.headers['authorization'];    
