b7b50c0af39decf510fb5550e9b1d8052b822e09
chris <christoph21.schweiger@gmx.de>
2025-07-08 09:06:07 +0200

    form for new immos

---
+++ frontend/src/admin/AddImmoForm.js
@@ -0,0 +1,136 @@
+const AddImmoForm = (formData, handleChange) => {
+    return (
+    <form>
+        <label>Objektnr.</label>
+        <input type="number" name="objectnr" value={formData.objectnr} onChange={handleChange}></input>
+
+        <label>Stadt</label>
+        <input
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+        />
+
+        <label>Beschreibung</label>
+        <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+        />
+
+        <label>Haus?</label>
+        <input
+            type="checkbox"
+            name="house"
+            checked={formData.house}
+            onChange={handleChange}
+        />
+
+        <label>Preis (€)</label>
+        <input
+            type="number"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+        />
+
+        <label>Fläche (m²)</label>
+        <input
+            type="number"
+            name="squareMeters"
+            value={formData.squareMeters}
+            onChange={handleChange}
+        />
+
+        <label>Bad vorhanden?</label>
+        <input
+            type="checkbox"
+            name="bathroom"
+            checked={formData.bathroom}
+            onChange={handleChange}
+        />
+
+        <label>Supermarkt in der Nähe?</label>
+        <input
+            type="checkbox"
+            name="supermarket"
+            checked={formData.supermarket}
+            onChange={handleChange}
+        />
+
+        <label>Stromversorgung?</label>
+        <input
+            type="checkbox"
+            name="electricity"
+            checked={formData.electricity}
+            onChange={handleChange}
+        />
+
+        <label>Wasserversorgung?</label>
+        <input
+            type="checkbox"
+            name="water"
+            checked={formData.water}
+            onChange={handleChange}
+        />
+
+        <label>Pool?</label>
+        <input
+            type="checkbox"
+            name="pool"
+            checked={formData.pool}
+            onChange={handleChange}
+        />
+
+        <label>Garten?</label>
+        <input
+            type="checkbox"
+            name="garden"
+            checked={formData.garden}
+            onChange={handleChange}
+        />
+
+        <label>Direkt am Meer?</label>
+        <input
+            type="checkbox"
+            name="atOcean"
+            checked={formData.atOcean}
+            onChange={handleChange}
+        />
+
+        <label>Zahlungsdauer (Monate)</label>
+        <input
+            type="number"
+            name="paymentDuration"
+            value={formData.paymentDuration}
+            onChange={handleChange}
+        />
+
+        <label>Baujahr (MM.YYYY)</label>
+        <input
+            type="text"
+            name="buildingFinished"
+            value={formData.buildingFinished}
+            onChange={handleChange}
+            placeholder="MM.YYYY"
+        />
+
+        <label>Über das Projekt</label>
+        <textarea
+            name="uberDasProjekt"
+            value={formData.uberDasProjekt}
+            onChange={handleChange}
+        />
+
+        <label>Über den Standort</label>
+        <textarea
+            name="uberStandort"
+            value={formData.uberStandort}
+            onChange={handleChange}
+        />
+    </form>
+    )
+}
+
+export default AddImmoForm;
--- frontend/src/admin/Immobiliencenter.js
+++ frontend/src/admin/Immobiliencenter.js
@@ -1,6 +1,58 @@
+import { useState } from "react";
+import AddImmoForm from "./AddImmoForm";
+
 const Immobiliencenter = () => {
+    const [formActive, setFormActive] = useState(false);
+   
+    const [formData, setFormData] = useState({
+        objectnr: 0,
+        city: "",
+        description: "",
+        house: true,
+        price: 0,
+        squareMeters: 0,
+        bathroom: true,
+        supermarket: true,
+        electricity: true,
+        water: true,
+        pool: true,
+        garden: true,
+        atOcean: true,
+        paymentDuration: 0,
+        buildingFinished: "01.2026",
+        uberDasProjekt: "",
+        uberStandort: ""
+    });
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormData((previous) => ({
+            ...previous,
+            [name]: type === "checkbox" ? checked : value
+        }))
+    }
+
+    if(formActive){
+        return (
+            <>
+                <h1>Immobiliencenter</h1>
+
+                <h2>Objekte hinzufügen</h2>
+
+                <AddImmoForm formData={formData} handleChange={handleChange}/>
+
+                <button onClick={() => setFormActive(false)}>Abbruch</button>
+                <button onClick={() => setFormActive(false)}>Speichern</button>
+            </>
+        )
+    }
+
     return (
+        <>
         <h1>Immobiliencenter</h1>
+
+        <button onClick={() => setFormActive(true)}>Neues Objekt hinzufügen</button>
+        </>
     )
 }
 
