8c0d30541a0d4287d706645d4c21c37c010b127c
chris <christoph21.schweiger@gmx.de>
2025-08-04 21:24:05 +0200

    social-media-anbindung (not final)

---
+++ frontend/src/nonadmin/components/SocialMediaAnbindung.js
@@ -0,0 +1,17 @@
+const SocialMediaAnbindung = () => {
+    return ( 
+        <>
+            <div>
+                <a href="mailto:meier-finanz@gmx.de">
+                    <img src="float_mail.svg" alt="Send Email" />
+                </a>
+                
+                <Link to="https://www.instagram.com/fina_ncecenterl/"><img src="insta.png"></img></Link>
+                <Link to="https://www.youtube.com/@InvestmentZypernmitFinan-fq2xo"><img src="youtube.png"></img></Link>
+                <Link to="https://www.tiktok.com/@finanz.center?_t=8nTRPfE3ejp&_r=%201"><img src="tiktok.svg"></img></Link>
+            </div>
+        </>
+    )
+}
+
+export default SocialMediaAnbindung;
