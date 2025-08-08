253a4b354fe9afaf340237e180f38895ea2719eb
chris <christoph21.schweiger@gmx.de>
2025-08-08 02:29:11 +0200

    Tailwindcss for Footer

--- frontendVite/src/nonadmin/components/Footer.jsx
+++ frontendVite/src/nonadmin/components/Footer.jsx
@@ -3,23 +3,26 @@ import { Link, Outlet } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="fc-footer">
+    <footer className="bg-[#093B3F] pl-10 pr-10">
 
-      <div className="fc-container">
-        <div id="links">
-            <Link className="linkComp" to="/impressum">Impressum</Link>          
-            <Link className="linkComp" to="/kontakt">Kontakt</Link>
-            <Link className="linkComp" to="/datenschutzerklarung">Datenschutz</Link>
+      <div className="md:flex h-30 justify-between items-center">
+        <div className="flex md:w-[20%] space-x-4 max-md:justify-around max-md:mb-3">
+            <Link className="text-white text-lg" to="/impressum">Impressum</Link>          
+            <Link className="text-white text-lg" to="/kontakt">Kontakt</Link>
+            <Link className="text-white text-lg" to="/datenschutzerklarung">Datenschutz</Link>
         </div>
-        <img id="logo" src='/logo-hell.webp'></img>
-        <div id="socialmedia">
-        <img src='/insta.png'></img>
-        <img src='/youtube.png'></img>
-        <img src='/whatsapp.svg'></img>
-        <img src='/tiktok.svg'></img>
+
+        <div className="flex justify-around w-[20vw] max-md:hidden">
+          <img className="w-[70%]" src='/logo-hell.webp'></img>
+        </div>
+        <div className="flex h-12 md:w-[20%] md:justify-between max-md:justify-around">
+          <img src='/insta.png'></img>
+          <img src='/youtube.png'></img>
+          <img src='/whatsapp.svg'></img>
+          <img src='/tiktok.svg'></img>
         </div>
       </div>
-      <div className="fc-copy">
+      <div className="text-white text-md">
         © {new Date().getFullYear()} Finance Center Meier – Alle Rechte vorbehalten.
       </div>
     </footer>
