80b7dbfb8435503930039182dd29e81692b94001
chris <christoph21.schweiger@gmx.de>
2025-08-08 22:24:01 +0200

    Einstiegsvideo

--- frontendVite/src/nonadmin/homepage/Einstieg.jsx
+++ frontendVite/src/nonadmin/homepage/Einstieg.jsx
@@ -10,24 +10,23 @@ const Einstieg = () => {
 
     return (
         <>
-            <video className="backgroundvideo" autoPlay muted loop playsInline> 
-                <source src='/backgroundvideo.mp4' type='video/mp4'></source>
-            </video>
-
-            <div className="entraceWrapper">
-                <div className="itemWrapper">
-                    <div className="h1Wrapper">
+            <div className="w-[100vw]">
+                <video className="w-full fixed z-[-1] bg-center bg-cover" autoPlay muted loop playsInline> 
+                    <source src='/backgroundvideo.mp4' type='video/mp4'></source>
+                </video>
+            </div>
+            <div className="relative flex flex-col items-center justify-center h-150 row w-[100vw]">
+                    <div className="text-white text-3xl md:text-4xl font-bold text-center drop-shadow-lg">
                         <h1>IHRE EXPERTEN FÜR IMMOBILIEN UND INVESTITIONEN</h1>
                     </div>
-                    <div class="h2Wrapper">
+                    <div className="text-3xl text-center text-white font-bold mt-3">
                         <h2>Immobilien auf Zypern</h2>
                     </div>
-                    <div className="buttonWrapper">
-                        <button className="myButton" onClick={() => navigate('/immobilien')}>ALLE IMMOBILIEN</button>
-                        <button className="myButton" onClick={() => navigate('/wohnungen')}>WOHNUNGEN</button>
-                        <button className="myButton" onClick={() => navigate('/house')}>HÄUSER</button>
+                    <div className="flex justify-center gap-x-20 pt-15">
+                        <button className="bg-[#093B3F] text-white w-55 h-10 rounded-full text-lg" onClick={() => navigate('/immobilien')}>ALLE IMMOBILIEN</button>
+                        <button className="bg-[#093B3F] text-white w-55 h-10 rounded-full text-lg" onClick={() => navigate('/wohnungen')}>WOHNUNGEN</button>
+                        <button className="bg-[#093B3F] text-white w-55 h-10 rounded-full text-lg" onClick={() => navigate('/house')}>HÄUSER</button>
                     </div>
-                </div>
             </div>
         </>
     )
