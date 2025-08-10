f8dea253189014d492e944742df03758cf1a9f82
chris <christoph21.schweiger@gmx.de>
2025-08-10 04:02:20 +0200

    admin login tailwind

--- frontendVite/src/Login.jsx
+++ frontendVite/src/Login.jsx
@@ -35,15 +35,37 @@ const Login = () => {
     return (
         <>
             
-            <h1>Admin Login</h1>
-            <form onSubmit={handleForm}>
-                <label>username</label>
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                <label>password</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <input type="submit" />
-            </form>
+            <h1 className="text-4xl text-[#E9D3AD] text-center font-bold mt-3 mb-3">Admin Login</h1>
+            <form
+                onSubmit={handleForm}
+                className="bg-[#093B3F] p-8 rounded-lg w-[30vw] m-auto space-y-4 text-white"
+                >
+                <div>
+                    <label className="block mb-1">Username</label>
+                    <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className="w-full border border-white rounded-full px-3 py-2 bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
+                    />
+                </div>
 
+                <div>
+                    <label className="block mb-1">Password</label>
+                    <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full border border-white rounded-full px-3 py-2 bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
+                    />
+                </div>
+
+                <input
+                    type="submit"
+                    value="Login"
+                    className="w-full border border-white rounded-full px-3 py-2 text-[#093B3F] bg-white text-lg font-semibold hover:bg-gray-100 transition cursor-pointer"
+                />
+            </form>
             <p>{ message }</p>
         </>
     );
